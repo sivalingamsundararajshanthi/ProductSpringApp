2f3400d74b394a06654bef068f89a42667224fd9
sivalingam77 <lingams200@gmail.com>
2019-05-25 14:51:55 -0500

    get products and add product done

--- src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductService.java
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductService.java
@@ -1,6 +1,7 @@
 package com.hackerrank.eshopping.product.dashboard.controller;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,14 +23,23 @@ public class ProductService {
 	private ProductRepository productRepository;
 	
 	/*
-	 * This method queries the database for all the products and appends them to a list and returns them as a list
+	 * This method queries the database for all the products and appends them to a list and returns them as a ResponseEntity with
+	 * a generic type of list of products.
 	 * 
-	 * Needs work
+	 * The returned list is in ascending order of id
 	 */
-	public List<Product> getAllProducts(){
+	public ResponseEntity<List<Product>> getAllProducts(){
+		
 		List<Product> products = new ArrayList<>();
+		
+		//Add all products from iterable
 		productRepository.findAll().forEach(products::add);
-		return products;
+		
+		//Sort the products using id
+		products.sort(Comparator.comparing(Product::getId));
+		
+		//return the response entity along with the Http response code
+		return new ResponseEntity<>(products, HttpStatus.OK);
 	}
 	
 	/*
--- src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductsController.java
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductsController.java
@@ -1,11 +1,69 @@
 package com.hackerrank.eshopping.product.dashboard.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hackerrank.eshopping.product.dashboard.model.Product;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductsController {
 
+	@Autowired
+	private ProductService productService;
 	
+	/*
+	 * This service method is used to get all the products in the database and return it with the response code
+	 */
+	public ResponseEntity<List<Product>> getAllProducts(){
+		return productService.getAllProducts();
+	}
+	
+	/*
+	 * This service method is used to add a product to the database
+	 */
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<?> addProduct(@RequestBody Product product) {
+		return productService.addProduct(product);
+	}
+	
+	/*
+	 * This service method is used to update a value in the database
+	 */
+	@RequestMapping(method=RequestMethod.PUT, value="/{product_id}")
+	public void updateProduct(@RequestBody Product product, @PathVariable Long product_id) {
+		
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
