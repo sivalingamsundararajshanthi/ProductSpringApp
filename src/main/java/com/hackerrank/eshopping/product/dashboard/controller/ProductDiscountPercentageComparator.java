f3d424857c951fb95fb4b2c532abe663df23f4ca
sivalingam77 <lingams200@gmail.com>
2019-05-26 18:54:59 -0500

    100

--- src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductDiscountPercentageComparator.java
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductDiscountPercentageComparator.java
@@ -8,28 +8,28 @@ public class ProductDiscountPercentageComparator implements Comparator<Product> 
 	
 	@Override
 	public int compare(Product p1, Product p2) {
-//		int discountPercentage = getDiscountedPercentage(p2) - getDiscountedPercentage(p1);
-//		
-//		if(discountPercentage != 0) {
-//			return discountPercentage;
-//		} else {
-//			int discountPriceComparison = Double.compare(p1.getDiscountedPrice(), p2.getDiscountedPrice());
-//			
-//			if(discountPriceComparison != 0) {
-//				return discountPriceComparison;
-//			} else {
-//				return Math.toIntExact(p1.getId()) - Math.toIntExact(p2.getId());
-//			}
-//		}
+		int discountPercentage = getDiscountedPercentage(p2) - getDiscountedPercentage(p1);
 		
-		return getDiscountedPercentage(p2) - getDiscountedPercentage(p1);
+		if(discountPercentage != 0) {
+			return discountPercentage;
+		} else {
+			int discountPriceComparison = Double.compare(p1.getDiscountedPrice(), p2.getDiscountedPrice());
+			
+			if(discountPriceComparison != 0) {
+				return discountPriceComparison;
+			} else {
+				return Math.toIntExact(p1.getId()) - Math.toIntExact(p2.getId());
+			}
+		}
+		
+//		return getDiscountedPercentage(p2) - getDiscountedPercentage(p1);
 	}
 	
 	private int getDiscountedPercentage(Product p) {
 		Double num = p.getRetailPrice() - p.getDiscountedPrice();
 		Double den = p.getRetailPrice();
 		Double div = num/den;
-		return (int) Math.round(div);
+		return (int) Math.round(div * 100);
 	}
 
 }
